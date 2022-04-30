a1bed4781c92c45aa46b931842250c31bc00afa0
Millzy <millzyyt08@gmail.com>
2022-04-30 17:24:59 +0100

    logger wrapper

--- ModList/Logger.cs
+++ ModList/Logger.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using QModLogger = QModManager.Utility.Logger;
 
 namespace ModList
 {
-    internal class Logger
+
+    /// <summary>
+    /// This is a wrapper around QMod's logger because I dont like the level param
+    /// </summary>
+    public class Logger
     {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        private Logger() {}
+
+        public static Logger Instance
+        {
+            get => _instance.Value;
+        }
+
+        public static void LogDebug(string msg = null, Exception ex = null, bool showOnScreen = false)
+            => QModLogger.Log(QModLogger.Level.Debug, msg, ex, showOnScreen);
+
+        public static void LogInfo(string msg = null, Exception ex = null, bool showOnScreen = false)
+            => QModLogger.Log(QModLogger.Level.Debug, msg, ex, showOnScreen);
+
+        public static void LogWarn(string msg = null, Exception ex = null, bool showOnScreen = false)
+            => QModLogger.Log(QModLogger.Level.Debug, msg, ex, showOnScreen);
+
+        public static void LogError(string msg = null, Exception ex = null, bool showOnScreen = false)
+            => QModLogger.Log(QModLogger.Level.Debug, msg, ex, showOnScreen);
+
+        public static void Logatal(string msg = null, Exception ex = null, bool showOnScreen = false)
+            => QModLogger.Log(QModLogger.Level.Debug, msg, ex, showOnScreen);
     }
 }
